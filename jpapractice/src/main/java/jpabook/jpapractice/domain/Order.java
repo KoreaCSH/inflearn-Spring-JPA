fedb539458330aebc81d3f3108fa4e3943873537
csh <tmdgjs7495@naver.com>
2023-03-13 17:42:42 +0900

    Member Domain 개발

--- jpapractice/src/main/java/jpabook/jpapractice/domain/Order.java
+++ jpapractice/src/main/java/jpabook/jpapractice/domain/Order.java
@@ -17,14 +17,17 @@ public class Order {
     @Column(name = "order_id")
     private Long id;
 
-    @ManyToOne
+    // ManyToOne은 기본이 즉시로딩이기 때문에 수동으로 지연로딩으로 설정해 주어야 한다.
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id") // foreign_key 가 member_id로 설정된다. + 연관관계의 주인이 된다.
     private Member member;
 
-    @OneToMany(mappedBy = "order")
+    // Order에서 orderItem 저장 시 OrderItem 따로 저장할 필요 X
+    // 즉, Order 에서 persist 하면 OrderItem 도 persist 된다.
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
     private List<OrderItem> orderItems = new ArrayList<>();
 
-    @OneToOne
+    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     @JoinColumn(name = "delivery_id")
     private Delivery delivery;
 
@@ -32,5 +35,22 @@ public class Order {
 
     @Enumerated(EnumType.STRING)
     private OrderStatus status; // 주문상태 [ Order, Cancle ]
+
+    // -- 연관관계 편의 메서드 --
+    // 양방향 관계일 때 사용하면 편하다. 메서드 하나로 해결 가능
+    public void setMember(Member member) {
+        this.member = member;
+        member.getOrders().add(this);
+    }
+
+    public void addOrderItem(OrderItem orderItem) {
+        orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
+
+    public void setDelivery(Delivery delivery) {
+        this.delivery = delivery;
+        delivery.setOrder(this);
+    }
 
 }
---
+++ jpapractice/src/test/java/jpabook/jpapractice/service/MemberServiceTest.java
@@ -0,0 +1,55 @@
+package jpabook.jpapractice.service;
+
+import jpabook.jpapractice.domain.Member;
+import jpabook.jpapractice.repository.MemberRepository;
+import jpabook.jpapractice.repository.MemberService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+class MemberServiceTest {
+
+    @Autowired
+    MemberService memberService;
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    void 회원가입() {
+        // given
+        Member member = new Member();
+        member.setName("kim");
+
+        // when
+        memberService.join(member);
+
+        // then
+        Member findMember = memberService.findOnd(member.getId());
+        Assertions.assertThat(member).isEqualTo(findMember);
+    }
+
+    @Test
+    void 중복_회원_예외() {
+        // given
+        Member member1 = new Member();
+        member1.setName("kim");
+
+        Member member2 = new Member();
+        member2.setName("kim");
+
+        // when
+        memberService.join(member1);
+
+        // then
+        assertThrows(IllegalStateException.class,
+                () -> memberService.join(member2));
+    }
+
+}
